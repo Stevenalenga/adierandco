d4fa11b9d46d34c5d945753b52c522580342546f
stevenalenga <stevenalenga@gmail.com>
2024-12-17 13:26:56 +0300

    "server actions"

--- next.config.js
+++ next.config.js
@@ -2,11 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
 
-  experimental: {
-    serverActions: {
-      allowedOrigins: ['localhost:3000'],
-    },
+    experimental: {
+    serverActions: true,
   },
+
 }
 
 export default nextConfig;
