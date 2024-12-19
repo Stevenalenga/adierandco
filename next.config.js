1ab3004cde73f09eb32c49d66a8ff2c6b805594e
stevenalenga <stevenalenga@gmail.com>
2024-12-19 16:35:06 +0300

    "config files"

--- next.config.js
+++ next.config.js
@@ -7,6 +7,23 @@ const nextConfig = {
       allowedOrigins: ['localhost:3000'],
     },
   },
-}
+
+ 
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'static/fonts/',
+          publicPath: '/_next/static/fonts/',
+        },
+      },
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
